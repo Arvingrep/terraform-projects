148f2eec8cbbdcb25ae268b42825ac41aa72430a
arvin <arvingrep@gmail.com>
2023-12-26 20:29:48 +0800

    first dev

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,9 +1,16 @@
 terraform {
   required_version = ">=0.12.0"
-  backend "s3" {
-    region  = "us-east-2"
-    profile = "default"
-    key     = "terraformstatefile"
-    bucket  = ""
+  # backend "s3" {
+  #   region  = "us-east-2"
+  #   profile = "default"
+  #   key     = "terraformstatefile"
+  #   bucket  = ""
+  # }
+
+  cloud {
+    organization = "2up"
+    workspaces {
+      name = "eks-cloud"
+    }
   }
 }
